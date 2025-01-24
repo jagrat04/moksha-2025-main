d172aa1c47c6d37a7aef3281f9af3554c6a6296b
MohitMaulekhi <mohitmaulekhi312@gmail.com>
2025-01-25 00:03:13 +0530

    added custom cursor

---
+++ src/components/CustomCursor.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import React, { useEffect, useRef, useState, useCallback } from 'react';
+const CustomCursor = () => {
+  const cursorRef = useRef<HTMLDivElement | null>(null);
+  const [position, setPosition] = useState<{ x: number, y: number }>({ x: 0, y: 0 });
+  const [visible, setVisible] = useState<boolean>(true);
+  const [isMouseConnected, setIsMouseConnected] = useState<boolean>(false);
+
+  const handleMouseMove = 
+    (e: MouseEvent) => {
+      setIsMouseConnected(true);
+      if (!visible) setVisible(true);
+      
+      const newPosition = { x: e.clientX, y: e.clientY };
+      setPosition(newPosition);
+    };
+
+  const handleMouseOut = (e: MouseEvent) => {
+    if (!e.relatedTarget) {
+      setVisible(false);
+    }
+  };
+  const handleMouseEnter = (e: MouseEvent) => {
+    if (!e.relatedTarget) {
+      setVisible(true);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+    window.addEventListener("mouseout", handleMouseOut);
+    window.addEventListener("mouseenter", handleMouseEnter);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseout", handleMouseOut);
+    };
+  }, [handleMouseMove]);
+
+  if (!isMouseConnected) {
+    return null;
+  }
+
+  return (
+    <div
+      style={{
+        top: position.y,
+        left: position.x,
+        backgroundImage: `url(/assets/cursor/phoenix.gif)`,
+        backgroundSize: 'cover',
+        width: '200px',
+        height: '200px',
+        display: visible ? 'block' : 'none',
+      }}
+      ref={cursorRef}
+      className="fixed pointer-events-none transition-transform -translate-x-1/2 -translate-y-1/2 z-50"
+    />
+  );
+};
+
+export default CustomCursor;
