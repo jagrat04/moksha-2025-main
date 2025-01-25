b08a521bcc5bd2407891778ce8b61f4c2992e279
Manas Madan <manasmadan08@gmail.com>
2025-01-25 23:27:26 +0530

    fixes

--- src/components/CustomCursor.tsx
+++ src/components/CustomCursor.tsx
@@ -1,36 +1,39 @@
-'use client'
+"use client";
 
-import React, { useEffect, useRef, useState, useCallback } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from "react";
 const CustomCursor = () => {
   const cursorRef = useRef<HTMLDivElement | null>(null);
-  const [position, setPosition] = useState<{ x: number, y: number }>({ x: 0, y: 0 });
+  const [position, setPosition] = useState<{ x: number; y: number }>({
+    x: 0,
+    y: 0,
+  });
   const [visible, setVisible] = useState<boolean>(true);
   const [isMouseConnected, setIsMouseConnected] = useState<boolean>(false);
 
-  const handleMouseMove = 
-    (e: MouseEvent) => {
-      setIsMouseConnected(true);
-      if (!visible) setVisible(true);
-      
-      const newPosition = { x: e.clientX, y: e.clientY };
-      setPosition(newPosition);
-      var arr = [1, 0.9, 0.8, 0.5, 0.2];
+  const handleMouseMove = (e: MouseEvent) => {
+    setIsMouseConnected(true);
+    if (!visible) setVisible(true);
 
-      arr.forEach(function(i) {
-        var x = (1 - i) * 75;
-        var star = document.createElement('div');
+    const newPosition = { x: e.clientX, y: e.clientY };
+    setPosition(newPosition);
+    var arr = [1, 0.9, 0.8, 0.5, 0.2];
 
-        star.className = 'star';
-        star.style.top = e.clientY + Math.round(Math.random() * x - x / 2) + 'px';
-        star.style.left = e.clientX + Math.round(Math.random() * x - x / 2) + 'px';
+    arr.forEach(function (i) {
+      var x = (1 - i) * 75;
+      var star = document.createElement("div");
 
-        document.body.appendChild(star);
+      star.className = "star";
+      star.style.top = e.clientY + Math.round(Math.random() * x - x / 2) + "px";
+      star.style.left =
+        e.clientX + Math.round(Math.random() * x - x / 2) + "px";
 
-        window.setTimeout(function() {
-          document.body.removeChild(star);
-        }, Math.round(Math.random() * i * 600));
-      });
-    };
+      document.body.appendChild(star);
+
+      window.setTimeout(function () {
+        document.body.removeChild(star);
+      }, Math.round(Math.random() * i * 600));
+    });
+  };
 
   const handleMouseOut = (e: MouseEvent) => {
     if (!e.relatedTarget) {
@@ -41,7 +44,7 @@ const CustomCursor = () => {
     if (!e.relatedTarget) {
       setVisible(true);
     }
-  }
+  };
 
   useEffect(() => {
     window.addEventListener("mousemove", handleMouseMove);
@@ -63,10 +66,10 @@ const CustomCursor = () => {
         top: position.y + 35,
         left: position.x - 60,
         backgroundImage: `url(/assets/cursor/wand.gif)`,
-        
-        width: '167px',
-        height: '93px',
-        display: visible ? 'block' : 'none',
+        transform: "scaleX(-1) translate(-22%, -50%)",
+        width: "167px",
+        height: "93px",
+        display: visible ? "block" : "none",
       }}
       ref={cursorRef}
       className="fixed pointer-events-none bg-cover transition-transform -translate-x-1/2 -translate-y-1/2 z-50"
